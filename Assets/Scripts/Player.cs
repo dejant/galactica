7173561640aed54088a1a01a5f43634bb724069f
dejan tomic <deejan.t@gmail.com>
2020-03-03 21:58:05 +0100

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    public float xySpeed = 20;
+
+    // Start is called before the first frame update 
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        LocalMove(h, v, xySpeed);
+        ClampPosition();
+    }
+
+    void LocalMove(float x, float y, float speed) {
+        transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+    }
+
+    void ClampPosition() {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);     
+        pos.x = Mathf.Clamp(transform.localPosition.x, -8f, 8f);
+        pos.y = Mathf.Clamp(transform.localPosition.y, -4f, 4f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    [Tooltip("meter pro seconds")] [SerializeField] float speed = 30f;
+    [Tooltip("meter")] [SerializeField] float xRange = 10f;
+    [Tooltip("meter")] [SerializeField] float yRange = 7f;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        updatePosition();
+
+    }
+
+    void updatePosition(){
+        float xInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yInput = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xInput * speed * Time.deltaTime;
+        float yOffset = yInput * speed * Time.deltaTime;
+
+        float rawPosX = transform.localPosition.x + xOffset;
+        float rawPosY = transform.localPosition.y + yOffset;
+
+        float clampedPosX = Mathf.Clamp(rawPosX, -xRange, xRange);
+        float clampedPosY = Mathf.Clamp(rawPosY, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedPosX, clampedPosY, transform.localPosition.z);
+    }
+
+}
